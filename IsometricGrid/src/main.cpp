22dda12b3ddbe5e7ba2985308e8f4166d0794c37
Gustavo Machado <gustavomf02@gmail.com>
2023-01-16 21:26:23 -0300

    Adiciona clique e toggle

--- IsometricGrid/src/main.cpp
+++ IsometricGrid/src/main.cpp
@@ -16,13 +16,17 @@
 int screenWidth = 500, screenHeight = 500; //largura e altura inicial da tela. Alteram com o redimensionamento de tela.
 
 int mx, my; //coordenadas do mouse
+bool mc = false; // Clique do mouse
 
 int blockSize = 100;
 int halfBlockSize = blockSize / 2;
 int quarterBlockSize = blockSize / 4;
 
+bool showGridIndex = false;
+
 void renderIsometricCube(float x, float y, int testeX, int testeY)
 {
+    // Desenha topo do cubo
     color(15);
 
     float topx[] = {x, x + halfBlockSize, x, x - halfBlockSize};
@@ -30,6 +34,7 @@ void renderIsometricCube(float x, float y, int testeX, int testeY)
 
     polygonFill(topx, topy, 4);
 
+    // Desenha lateral direita do cubo
     color(14);
 
     float frontx[] = {x, x + halfBlockSize, x + halfBlockSize, x};
@@ -37,6 +42,7 @@ void renderIsometricCube(float x, float y, int testeX, int testeY)
 
     polygonFill(frontx, fronty, 4);
 
+    // Desenha lateral esquerda do cubo
     color(16);
 
     float backx[] = {x, x - halfBlockSize, x - halfBlockSize, x};
@@ -44,16 +50,20 @@ void renderIsometricCube(float x, float y, int testeX, int testeY)
 
     polygonFill(backx, backy, 4);
 
+    // Desenha highlight das bordas do cubo
     color(0.78, 0.99, 0.93);
 
     line( x - halfBlockSize, y - quarterBlockSize, x, y);
     line( x + halfBlockSize, y - quarterBlockSize, x, y);
     line( x, y + halfBlockSize, x, y);
 
-    color(2);
+    if (showGridIndex){
+       // Desenha coordenada do cubo na matriz
+        color(2);
 
-    text(x, y, testeX);
-    text(x, y + 10, testeY);
+        text(x, y, testeX);
+        text(x, y + 10, testeY);
+    }
 }
 
 float xMatrixToScreen(int x, int y)
@@ -76,27 +86,36 @@ float yScreenToMatrix(int x, int y)
     return (x * -1.0 / blockSize + y * 2.0 / blockSize);
 }
 
+bool isMouseOverBlock(int x, int y){
+    float dx =  xScreenToMatrix(mx, my);
+    float dy =  yScreenToMatrix(mx, my);
+
+    float xDiff = x-dx;
+    float yDiff = y-dy;
+
+    return (xDiff > 0 && xDiff < 1 && yDiff > 0 && yDiff < 1);
+}
+
 int offSet = 0;
 
 //funcao chamada continuamente. Deve-se controlar o que desenhar por meio de variaveis
 //globais que podem ser setadas pelo metodo keyboard()
 void render()
 {
-    float dx =  xScreenToMatrix(mx, my);
-    float dy =  yScreenToMatrix(mx, my);
-
     for (int x = 0; x <= 40; x++){
         for (int y = 0; y <= 5; y++){
-            float xDiff = x-dx;
-            float yDiff = y-dy;
-
             offSet = 0;
 
-            if (xDiff > 0 && xDiff < 1 && yDiff > 0 && yDiff < 1){
-                offSet = quarterBlockSize;
+            if (isMouseOverBlock(x, y)){
+                if (mc)
+                {
+                    offSet = halfBlockSize;
+                } else {
+                    offSet = -quarterBlockSize;
+                }
             }
 
-            renderIsometricCube(xMatrixToScreen(x, y), yMatrixToScreen(x, y) - offSet, x, y);
+            renderIsometricCube(xMatrixToScreen(x, y), yMatrixToScreen(x, y) + offSet, x, y);
         }
     }
 
@@ -104,12 +123,20 @@ void render()
 
     color(0, 0, 0, 0.1);
     circleFill( mx, my, 10, 10);
+
+    text(50, screenHeight - 50, "1 - Toggle grid index");
 }
 
 //funcao para tratamento de mouse: cliques, movimentos e arrastos
 void mouse(int button, int state, int wheel, int direction, int x, int y)
 {
-   //printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
+    //printf("\nmouse %d %d %d %d %d %d", button, state, wheel, direction,  x, y);
+
+    if (state == 0){
+        mc = true;
+    } else {
+        mc = false;
+    }
 
    mx = x; //guarda as coordenadas do mouse para exibir dentro da render()
    my = y;
@@ -118,6 +145,10 @@ void mouse(int button, int state, int wheel, int direction, int x, int y)
 //funcao chamada toda vez que uma tecla for pressionada
 void keyboard(int key)
 {
+   if (key == 49) {
+        showGridIndex = !showGridIndex;
+   }
+
    printf("\nClicou Tecla: %d" , key);
 }
 
